5fcf8b80b24bd4698536e0f2daac905a4b0d8f2c
jeelsakhiya09 <jeelsakhiya95@gmail.com>
2024-06-06 10:22:49 +0530

    updated '/profile' route in 'routes/user.js'

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middlewares/authentication.js";
 import { handleUserSignIn, handleUserSignUp } from "../controllers/user.js";
+import User from "../models/user.js";
 
 const router = Router();
 
 router.post("/register", handleUserSignUp);
 router.post("/login", handleUserSignIn);
-router.get("/profile", isLoggedIn, (req, res) => {
-    return res.send("You are loggedIn");
+router.get("/profile", isLoggedIn, async (req, res) => {
+    const user = await User.findById(req.user.id);
+    return res.json({user });
 })
 router.get("/logout", isLoggedIn, (req, res) => {
     return res.clearCookie("accessToken").json({
